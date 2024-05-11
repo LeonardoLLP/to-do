7791b4cfed71e8e9f5fd5622690d798d68b0f334
Leonardo <linop3132@gmail.com>
2024-05-11 15:55:53 +0200

    Refactor cookies functions to new file

---
+++ cookies.js
@@ -0,0 +1,38 @@
+"use strict";
+
+//TODO: Need to reference listArray. how to share between files?
+//TODO: Export addItem from todo and import it here.
+
+// Cookies to save list.
+function saveCookieList() {
+    let s = "";
+    for (let item of listArray) {
+        s += item.querySelector("input").value;
+        s += "&";
+    }
+    s = s.substring(0, s.length - 1);
+    document.cookie = `todoList=${encodeURIComponent(s)}; max-age=10000000`;
+}
+
+function getCookieList() {
+    let all = document.cookie;
+    all = decodeURIComponent(all);
+    let list = all.split("; ");
+    for (let cookie of list) {
+        if (!cookie.includes("=")) continue;
+        let p = cookie.indexOf("=");
+        let name = cookie.substring(0, p);
+        if (name != "todoList") continue;
+        let list = cookie.substring(p + 1);
+        let items = list.split("&");
+        for (let item of items) {
+            if (item == "") continue;
+            let todo_item = addItem();
+            todo_item.text.value = item;
+        }
+        break;
+    }
+}
+
+window.addEventListener("load", getCookieList);
+window.addEventListener("beforeunload", saveCookieList);
--- todo.js
+++ todo.js
@@ -83,38 +83,3 @@ let addItem = (() => {
 })
 
 addButton.addEventListener("click", addItem)
-
-
-// Cookies to save list.
-function saveCookieList() {
-    let s = "";
-    for (let item of listArray) {
-        s += item.querySelector("input").value;
-        s += "&";
-    }
-    s = s.substring(0, s.length-1);
-    document.cookie = `todoList=${encodeURIComponent(s)}; max-age=10000000`;
-}
-
-function getCookieList() {
-    let all = document.cookie;
-    all = decodeURIComponent(all);
-    let list = all.split("; ");
-    for (let cookie of list) {
-        if (!cookie.includes("=")) continue;
-        let p = cookie.indexOf("=");
-        let name = cookie.substring(0, p);
-        if (name != "todoList") continue;
-        let list = cookie.substring(p+1);
-        let items = list.split("&");
-        for (let item of items) {
-            if (item == "") continue;
-            let todo_item = addItem();
-            todo_item.text.value = item;
-        }
-        break;
-    }
-}
-
-window.addEventListener("load", getCookieList);
-window.addEventListener("beforeunload", saveCookieList);
